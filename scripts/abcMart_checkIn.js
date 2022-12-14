342a18a1aab281602bf4c0bd7f9cb710a921cd08
liao <liao@Developde-Mac-mini.local>
2022-12-14 09:42:18 +0800

    修正錯誤

--- scripts/abcMart_checkIn.js
+++ scripts/abcMart_checkIn.js
@@ -1,7 +1,3 @@
-const abcMartHeaders = {
-    'authorization': $persistentStore.read('AbcMartToken'),
-  };
-  
   function abcMartNotify(subtitle = '', message = '') {
     $notification.post('ABC Mart 簽到', subtitle, message, {
       'url': ''
@@ -9,13 +5,6 @@ const abcMartHeaders = {
   };
   
   const checkinRequest = {
-    url: 'https://www.abc-mart.com.tw/api/auth/coupons',
-    headers: {
-      'authorization': $persistentStore.read('AbcMartToken'),
-    }
-  };
-  
-  const historyRequest = {
     url: 'https://www.abc-mart.com.tw/api/home',
     headers: {
       'authorization': $persistentStore.read('AbcMartToken'),
@@ -36,52 +25,6 @@ const abcMartHeaders = {
             try {
               if (obj.status === 1) {
                 abcMartNotify(
-                  '簽到成功 ✅',
-                  '獲得 ' + obj.result.length + ' 積分'
-                );
-              } else if (obj.status === 2) {
-                abcMartNotify(
-                  '簽到失敗 ‼️',
-                  '今日已簽到'
-                );
-              } else {
-                abcMartNotify(
-                  '簽到失敗 ‼️',
-                  'Error: ' + obj.message
-                );
-              }
-            } catch (e) {
-              abcMartNotify(
-                '簽到失敗 ‼️',
-                e
-              );
-            }
-          checkinHistory();
-        } else {
-          abcMartNotify(
-            'Cookie 已過期 ‼️',
-            '請重新登入'
-          );
-          $done();
-        }
-      }
-      $done();
-    });
-  }
-  
-  function checkinHistory() {
-    $httpClient.get(historyRequest, function (error, response, data) {
-      if (error) {
-        abcMartNotify(
-          '檢查點數紀錄失敗 ‼️',
-          '連線錯誤' + e
-        );
-      } else {
-        if (response.status === 200) {
-            const obj = JSON.parse(data);
-            try {
-              if (obj.status === 1) {
-                abcMartNotify(
                   '目前點數',
                   '獲得 ' + obj.result.user.get_bonus + ' 積分'
                 );
@@ -93,7 +36,7 @@ const abcMartHeaders = {
               }
             } catch (e) {
               abcMartNotify(
-                '檢查點數紀錄失敗 ‼️️',
+                '簽到失敗 ‼️',
                 e
               );
             }
@@ -102,10 +45,10 @@ const abcMartHeaders = {
             'Cookie 已過期 ‼️',
             '請重新登入'
           );
+          $done();
         }
       }
       $done();
     });
   }
-  
   checkin();
